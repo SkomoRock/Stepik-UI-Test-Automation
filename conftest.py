3c7445efdb1506a884610f9f4d78c00910707f13
SkomoRock <s.v.rodnikov@gmail.com>
2022-03-12 19:34:45 +0300

    Base files created

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action = 'store', default = None,
+                     help = "Choose language: ru or en")
+
+# only for chrome browser
+@pytest.fixture(scope = 'function')
+def browser(request):
+    user_language = request.config.getoption('language')
+    tuning = Options()
+    tuning.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    # turn off notifications DevTools
+    tuning.add_experimental_option('excludeSwitches', ['enable-logging'])
+    browser = webdriver.Chrome(options = tuning)
+    yield browser
+    browser.quit()
