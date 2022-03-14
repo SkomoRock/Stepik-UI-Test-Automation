0546740dc45e3d920531445940d3ed825a694f1e
SkomoRock <s.v.rodnikov@gmail.com>
2022-03-14 16:48:11 +0300

    Main page test fix

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,5 +7,8 @@ class MainPage(BasePage):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Login link is NOT PRESENTED'
 
     def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        link.click()
+        try:
+            link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+            link.click()
+        except:
+            assert self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Go to login page is FAILED'
--- test_main_page.py
+++ test_main_page.py
@@ -7,27 +7,23 @@ from pages.login_page import LoginPage
 main_link = 'http://selenium1py.pythonanywhere.com'
 login_link = 'http://selenium1py.pythonanywhere.com/accounts/login'
 
-# checking for authorization link
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, main_link)
     page.open()
     page.should_be_login_link()
 
-# checking the transition to the authorization page from the main page
-def test_guest_should_see_login_page(browser):
+def test_guest_should_go_to_login_page(browser):
     page = MainPage(browser, main_link)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-# checking for the presence of an authorization form
 def test_guest_should_see_login_form(browser):
     page = LoginPage(browser, login_link)
     page.open()
     page.should_be_login_form()
 
-# checking for the presence of a registration form
 def test_guest_should_see_register_form(browser):
     page = LoginPage(browser, login_link)
     page.open()
