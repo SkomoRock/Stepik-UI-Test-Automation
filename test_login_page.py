ef7f334c00acc0955fe76eabfac0b428c7769566
SkomoRock <s.v.rodnikov@gmail.com>
2022-03-14 11:26:12 +0300

    Added verification of the authorization page

---
+++ test_login_page.py
@@ -0,0 +1,21 @@
+# run command:
+# py -m pytest -v --tb=line --language=en test_login_page.py
+
+from pages.login_page import LoginPage
+
+link = 'http://selenium1py.pythonanywhere.com/accounts/login'
+
+def test_guest_should_see_login_url(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+def test_guest_should_see_login_form(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_should_see_register_form(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
